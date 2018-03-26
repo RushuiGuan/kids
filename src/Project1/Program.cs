96bf445762ee3fcb7fdd1f8dbb8113fe2a84d591
Rushui Guan <rushui@outlook.com>
2018-03-25 23:57:56 -0400

    update the program.cs

--- src/Project1/Program.cs
+++ src/Project1/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 
-namespace Project1
-{
-    public class Program
-    {
+namespace Project1 {
+	//double slash at the beginning of the line indicates that this line is a comment.  You can type anything on this line
+
+	public class Program
+	{ //sets of curly bracket { } indicates a scope, typically anything declared within a scope is only accessable within the scope.
+
 		static int Main(string[] args) {
 			Console.WriteLine("Hello World!");
 			return 0;
