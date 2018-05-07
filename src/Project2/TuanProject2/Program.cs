7f617de05fe9c78685f6c5bdc9b599e66447e043
phunghuytuan <37781789+phunghuytuan@users.noreply.github.com>
2018-05-06 22:33:59 -0400

    I changed the file Tuanproject2

--- src/Project2/TuanProject2/Program.cs
+++ src/Project2/TuanProject2/Program.cs
@@ -8,8 +8,29 @@ namespace TuanProject2
 {
 	class Program
 	{
-		static void Main(string[] args)
+		static int Main(string[] args)
 		{
+			int a = 1;
+			int b = 2;
+
+			int c = Sum(a, b);
+			Console.WriteLine(c);
+
+
+			a = 100;
+			b = 200;
+			c = Sum(a, b);
+			c += 2;
+			Console.WriteLine(c);
+
+			return c;
 		}
+	       static int Sum(int y, int x) {
+			int z;
+			z = x + y;
+			return z;
+
+		}
+
 	}
 }
