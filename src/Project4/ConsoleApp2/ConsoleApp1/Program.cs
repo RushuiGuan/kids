0d75e598d0c1330ae7f554b6ee7de409a98d05b2
phunghuytuan <37781789+phunghuytuan@users.noreply.github.com>
2018-06-17 22:12:02 -0400

    I changed the code !

--- src/Project4/ConsoleApp2/ConsoleApp1/Program.cs
+++ src/Project4/ConsoleApp2/ConsoleApp1/Program.cs
@@ -6,53 +6,32 @@ using System.Threading.Tasks;
 
 public class TuanProject4
 {
-	public static int Sum(int num1, int num2)
-	{
-		int total;
-		total = (num1 * num2) / 2 ; 
-		return total;
+	public static void GetTriangleArea() {
+
+		Console.Write("Please enter the height of the triangle (in^2): ");
+
+		string firstNumber;
+		firstNumber = Console.ReadLine();
+		double n1;
+		n1 = Convert.ToDouble(firstNumber);
+
+		Console.Write("\n\nPlease enter the base of the triangle (in^2): ");
+
+		string secondNumber;
+		secondNumber = Console.ReadLine();
+		double n2;
+		n2 = Convert.ToDouble(secondNumber);
+
+
+		double Area;
+		Area = n1 * n2 / 2;
+		Console.WriteLine("\nThe area of the triangle is : {0} in^2\n", Area);
+
 	}
 
 	public static void Main()
 	{
-
-
-		{
-			Console.Write("\n\nThis program will calculate the area of the triangle :\n");
-			Console.Write("__________________________________________________________\n");
-
-			Console.Write("Please enter the height of the triangle (in^2): ");
-
-			string Firstnumber;
-			Firstnumber = Console.ReadLine();
-			int n1;
-			n1 = Convert.ToInt32(Firstnumber);
-
-			Console.Write("\n\nPlease enter the base of the triangle (in^2): ");
-
-			string Secondnumber;
-			Secondnumber = Console.ReadLine();
-			int n2;
-			n2 = Convert.ToInt32(Secondnumber);
-
-			
-
-
-
-
-			
-
-
-
-
-			int Tuan;
-			Tuan = (n1 * n2) / 2 ;
-			Console.WriteLine("\nThe area is : {0} in^2\n", Tuan);
-			Firstnumber = Console.ReadLine();
-
-		}
-
-
+		GetTriangleArea();
 	}
 }
 
